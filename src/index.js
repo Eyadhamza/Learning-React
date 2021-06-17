59645b49d4bff9cbbccab1d3d79594d193ca7edd
EyadHamza <EyadHamza0@outlook.com>
2021-06-17 09:11:14 +0200

    Second example

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import Welcome from "./Welcome";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Clock from "./Clock";
+import Clock from "./components/Clock";
 import Input from "./Input"
 
 // let user = {
